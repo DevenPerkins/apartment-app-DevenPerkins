415f7cb321925551a3f10f5ce03e3a00508f7330
DevenPerkins <deven.s.perkins@gmail.com>
2021-06-15 09:30:48 -0700

    cleaning up the app

--- app/javascript/components/Components/Footer.test.js
+++ app/javascript/components/Components/Footer.test.js
@@ -8,12 +8,7 @@ import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16' 
 
  // Imports in the component we are going to be testing. 
-import CatIndex from '../CatIndex'
 
 //Allows us to utilize the adapter we import in earlier, allowing us to call and render a component. 
 Enzyme.configure({ adapter: new Adapter() }) 
 
-it('CatIndex renders content', () => {
-  const catIndex = shallow(<CatIndex />)
-  expect(catIndex.find('p').text()).toEqual('All the cats.')
-})
--- app/javascript/components/Pages/AptIndex.js
+++ app/javascript/components/Pages/AptIndex.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { NavLink } from 'react-router-dom';
 import { Jumbotron, Container } from 'reactstrap';
+import AptCards from "../Components/AptCards";
 
 class AptIndex extends Component {
   render() {
@@ -17,18 +18,20 @@ class AptIndex extends Component {
       <Jumbotron fluid  className="display-3">
         <Container className="-jumbo-container" fluid className="display-3">
         <h1 className="display-3">Here are the available apartments</h1>
-        <ul className="lead">
-          {this.props.apts.map((apt) => {
-            return (
-            <li key={apt.id}>
-                <NavLink to={`/aptshow/${apt.id}`}>{apt.street}</NavLink>
-            </li>
-            )
-          })}
-        </ul>
         </Container>
       </Jumbotron>
     </div>
+    <div>
+      <AptCards>
+            {this.props.apts.map((apt) => {
+              return (
+             <li key={apt.id}>
+                 <NavLink to={`/aptshow/${apt.id}`}>{apt.street}</NavLink>
+             </li>
+             )
+            })}
+          </AptCards>
+        </div>
       </>
     );
   }
