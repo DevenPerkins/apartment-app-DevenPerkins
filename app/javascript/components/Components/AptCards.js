86e89b66104d5d181a55fe18e518ed1c5ceeb5d9
DevenPerkins <deven.s.perkins@gmail.com>
2021-06-15 10:37:20 -0700

    spruce-up app

--- app/javascript/components/Components/AptCards.js
+++ app/javascript/components/Components/AptCards.js
@@ -1,49 +1,40 @@
 import React from 'react';
 import {
   Card, Button, CardImg, CardTitle, CardText, CardDeck,
-  CardSubtitle, CardBody
+  CardSubtitle, CardBody, CardGroup
 } from 'reactstrap';
 
 const AptCards = (props) => {
   return (
-    <CardDeck>
-      <Card>
-        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-    </CardDeck>
+    <>
+    <h3>Apartments</h3>
+    <CardGroup className="cards">
+        { this.props.apts.map(apt => {
+          return (
+            <Card sm="4" key={ apt.id }>
+              <div className="my-show-card">
+                <h4>Location</h4>
+                <h5>{ apt.street }</h5>
+                <h5>{ apt.city }, { apt.state }</h5>
+                <br />
+                <h4>Details</h4>
+                <h5>{ apt.price } per month</h5>
+                <h5>Bedrooms: { apt.bedrooms }</h5>
+                <h5>Bathrooms: { apt.bathrooms }</h5>
+                <h5>Pets Allowed: { apt.pets }</h5>
+                  <p className="my-card-button">
+                    <NavLink to={`/aptshow/${apt.id}`}>{apt.street}>
+                      <Button color="secondary">
+                        View Listing
+                      </Button>
+                    </NavLink>
+                  </p>
+              </div>
+            </Card>
+          )
+        })}
+    </CardGroup>
+    </>
     
     
   );
